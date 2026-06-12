e4a395435665e7272d7c0e8210a012da63295b48
Rakesh Chaudhary <arcsle09plus@gmail.com>
2026-06-12 11:24:33 +0000

    added rag pipeline

--- lib/load_ai_client.py
+++ lib/load_ai_client.py
@@ -1,11 +1,15 @@
-from dotenv import load_dotenv
 import anthropic
+from openai import OpenAI
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def instantiate_anthropic_client():
-    load_dotenv()
     client = anthropic.Anthropic(api_key=os.getenv("ANTHROPIC_API_KEY"))
     return client
 
 
 
+def instantiate_openai_client():
+    client = OpenAI()
+    return client
